6c0c30c6936bad13e0911c0187d7a07a51cca18a
Jeremy Clark <jeremy@Jeremys-MacBook-Pro.local>
2017-09-01 10:08:14 -0700

    Initial project

---
+++ Person.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CoreTaskApp
+{    
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Rating { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace CoreTaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One Moment Please");
+            var repository = new PersonRepository();
+
+            var peopleTask = repository.GetAsync(CancellationToken.None);
+
+            peopleTask.ContinueWith(task =>
+            {
+                var people = task.Result;
+                foreach(var person in people)
+                    Console.WriteLine(person.ToString());
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
