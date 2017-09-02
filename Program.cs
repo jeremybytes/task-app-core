edf714a6d08cde5efd083ad8c2b89c9d15195884
Jeremy Clark <jeremy@jeremybytes.com>
2017-09-01 20:03:00 -0700

    Added cancellation and error handling

--- Program.cs
+++ Program.cs
@@ -1,25 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CoreTaskApp
 {
     class Program
     {
+        private static CancellationTokenSource tokenSource;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("One Moment Please");
-            var repository = new PersonRepository();
+            tokenSource = new CancellationTokenSource();
 
-            var peopleTask = repository.GetAsync(CancellationToken.None);
+            Console.WriteLine("One Moment Please (press 'x' to Cancel)");
+
+            var repository = new PersonRepository();
+            var peopleTask = repository.GetAsync(tokenSource.Token);
 
             peopleTask.ContinueWith(task =>
-            {
-                var people = task.Result;
-                foreach(var person in people)
-                    Console.WriteLine(person.ToString());
-            });
+                {
+                    var people = task.Result;
+                    foreach(var person in people)
+                        Console.WriteLine(person.ToString());
+                    Environment.Exit(0);
+                },
+                TaskContinuationOptions.OnlyOnRanToCompletion);
 
-            Console.ReadLine();
+            peopleTask.ContinueWith(
+                HandleError,
+                TaskContinuationOptions.OnlyOnFaulted);
+
+            peopleTask.ContinueWith(
+                HandleCancellation,
+                TaskContinuationOptions.OnlyOnCanceled);
+
+            HandleExit();
+        }
+
+        private static void HandleError(Task<List<Person>> task)
+        {
+            Console.WriteLine("\nThere was a problem retrieving data");
+            Environment.Exit(1);
+        }
+
+        private static void HandleCancellation(Task<List<Person>> task)
+        {
+            Console.WriteLine("\nThe operation was canceled");
+            Environment.Exit(0);
+        }
+
+        private static void HandleExit()
+        {
+            ExitLoop:
+            if (Console.ReadKey().Key == ConsoleKey.X)
+            {
+                tokenSource.Cancel();
+                goto ExitLoop;
+            }
+            else
+            {
+                Console.WriteLine("Waiting...");
+                goto ExitLoop;
+            }
         }
     }
 }
